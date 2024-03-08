49dfc2a63d47a95dd1b9ad96f75f70cdf73d12f3
UrielC2004 <131821174+UFCampos@users.noreply.github.com>
2024-03-08 01:37:16 -0300

    Update Conclusion.tsx

--- components/Conclusion.tsx
+++ components/Conclusion.tsx
@@ -5,7 +5,7 @@ export const Conclusion: React.FC = () => {
     return (
       <Card>
         <Subtitle>Conclusión</Subtitle>
-        <p>En conclusión, el viaje de JavaScript desde un lenguaje de scripting centrado en el navegador hasta una potencia de pila completa ha sido notable. Su evolución continua y su adopción generalizada subrayan su importancia en el siempre cambiante mundo del desarrollo web. A medida que avanzamos, es probable que JavaScript siga siendo líder en innovación, impulsando la creación de experiencias digitales dinámicas y atractivas.</p>
+        <p>En conclusión, el viaje de JavaScript desde un lenguaje de scripting centrado en el navegador hasta una potencia fullstack ha sido notable. Su evolución continua y su adopción generalizada subrayan su importancia en el siempre cambiante mundo del desarrollo web. A medida que avanzamos, es probable que JavaScript siga siendo líder en innovación, impulsando la creación de experiencias digitales dinámicas y atractivas.</p>
       </Card>
     );
   };
